f9d83cb4de5794ceaffd0ec460f17e2974f86ee5
cinxini <kleaws@wni.com>
2024-06-11 14:24:10 +0900

    add vuetify

---
+++ plugins/vuetify.js
@@ -0,0 +1,11 @@
+import "@mdi/font/css/materialdesignicons.css";
+
+import { createVuetify } from "vuetify";
+import "vuetify/styles";
+
+export default defineNuxtPlugin((app) => {
+    const vuetify = createVuetify({
+        // ... your configuration
+    });
+    app.vueApp.use(vuetify);
+});
