91b55b7ad975e6763996dd7e14b5a1e87d9456b8
cinxini <kleaws@wni.com>
2024-06-11 15:50:35 +0900

    add navbar layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,40 @@
 <template>
   <v-app>
-    
+
     <v-app-bar :elevation="0">
-    <template v-slot:prepend>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-    </template>
-    <v-app-bar-title>Application Bar</v-app-bar-title>
+      <!-- <template v-slot:prepend>
+        <v-img
+    :width="151"
+    aspect-ratio="16/9"
+    cover
+    src="@/assets/images/logo.png"
+  ></v-img>
+      </template>
+      <v-app-bar-title class=" d-flex justify-center">Application Bar</v-app-bar-title> -->
+      <v-row no-gutters>
+      <v-col cols="2" class="d-flex flex-row">
+        <div class="vertical-center"><v-img
+          :width="151"
+          aspect-ratio="16/9"
+          cover
+          src="@/assets/images/logo.png"
+        ></v-img></div>
+        
+      </v-col>
+      <v-col  class="d-flex justify-center ">
+          <div class="vertical-center">
+            <v-btn color="primary">about</v-btn>
+            <v-btn color="primary">blog</v-btn>
+            <v-btn color="primary">portfolio</v-btn>
+        </div>
+      </v-col>
+      <v-col cols="2"  class="d-flex flex-row-reverse">
+        <div class="vertical-center">
+          <v-btn color="primary">b1</v-btn>
+          <v-btn color="primary">b2</v-btn>
+        </div>
+      </v-col>
+    </v-row>
     </v-app-bar>
 
     <v-main>
@@ -22,5 +51,8 @@ import { VApp } from 'vuetify/components';
 </script>
 
 <style>
-
+.vertical-center {
+  display: flex;
+  align-items: center;
+}
 </style>
