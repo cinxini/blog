37ce01830ab20d1996a6c3a0cda532949b1442cf
cinxini <kleaws@wni.com>
2024-06-11 14:42:44 +0900

    default layout

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    
+    <v-app-bar :elevation="0">
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </template>
+    <v-app-bar-title>Application Bar</v-app-bar-title>
+    </v-app-bar>
+
+    <v-main>
+    <slot />
+    </v-main>
+
+  </v-app>
+  
+</template>
+
+<script setup>
+import { VApp } from 'vuetify/components';
+
+</script>
+
+<style>
+
+</style>
