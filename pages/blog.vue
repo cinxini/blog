3b1385c0643dcc70a7aa6a6cb1302cb63e97a6c3
Nutnaree Kleawsirikul <kleaws@wni.com>
2024-07-08 21:44:36 +0900

    derive tags

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,5 +1,6 @@
 <script  setup>
 import ArticleCard from '@/components/blog/ArticleCard.vue';
+import FlexBox from '@/components/containers/FlexBox.vue';
 import c from '@/constants/blog';
 import { computed, ref, watch } from 'vue';
 import { useRouter } from 'vue-router';
@@ -29,9 +30,55 @@ watch(page, async (newPage, prevPage) => {
 })
 
 console.log('init blog posts', blogPosts)
+
+const allTags = await queryContent('/post').only(['tags']).find()
+const tagCount = {};
+allTags.forEach(tags => {
+  tags.tags.forEach(tag => {
+    if(tagCount[tag]) 
+      tagCount[tag]++; 
+    else 
+      tagCount[tag] = 1;
+  })
+})
+
+console.log(Object.entries(tagCount).sort((a, b) => { 
+  return  (a[1] > b[1] ? -1 : 1);
+}))
 </script>
 
 <template>
+  <v-navigation-drawer
+      permanent
+      app
+      floating
+      width="250" 
+      style="position:fixed; top:0; left:10; overflow-y: auto; "
+      variant="plain"
+      class="main-background"
+  >
+    <div style="padding-top: 50px; text-align: left;">  
+      <v-list-item>
+        <v-list-item-title>Category</v-list-item-title>
+        <v-list-item-subtitle>
+          <FlexBox style="flex-wrap: wrap;">
+            <v-chip label variant="outlined" density="comfortable" size="small" color="secondary-lighten-3">all</v-chip>
+            <v-chip label variant="outlined" density="comfortable" size="small" color="secondary-lighten-3">datasci</v-chip>
+            <v-chip label variant="outlined" density="comfortable" size="small" color="secondary-lighten-3">webdev</v-chip>
+            <v-chip label variant="outlined" density="comfortable" size="small" color="secondary-lighten-3">others</v-chip>
+          </FlexBox>
+        </v-list-item-subtitle>
+      </v-list-item>
+
+      <v-list-item>
+        <v-list-item-title>Tags</v-list-item-title>
+        <v-list-item-subtitle>
+          <FlexBox style="flex-wrap: wrap;"></FlexBox>
+        </v-list-item-subtitle>
+      </v-list-item>
+    </div>
+  
+  </v-navigation-drawer>
   <v-container style=" max-width: 800px; min-width: 400px;">
     <div v-if="blogPosts">
       <p class="text-center text-h5 poppins-regular">Recent Posts</p>
@@ -46,10 +93,7 @@ console.log('init blog posts', blogPosts)
       ></v-pagination>
   </div>
   </v-container>
-  
 
-  
-  
 </template>
 
 
