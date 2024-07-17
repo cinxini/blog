abd7678ba4caf04be4259c73a970020af8a79c39
Nutnaree Kleawsirikul <kleaws@wni.com>
2024-07-17 20:18:08 +0900

    update theme

--- components/blog/ArticleCard.vue
+++ components/blog/ArticleCard.vue
@@ -14,37 +14,35 @@ const date = useDate()
 const router = useRouter();
 </script>
 <template>
-  <v-card class="my-2 main-background"  @click="router.push(article._path)" variant="flat" >
-      <div class="ma-0 px-4 py-2">
-        <v-row no-gutters class="text-caption poppins-regular pb-2" >
-          <v-col class="d-flex flex-row  align-center">
-            <v-chip class="" label variant="outlined" density="comfortable" size="small" color="secondary-lighten-3">
-              {{ article.category }}
-            </v-chip>
-          </v-col>
-        </v-row>
-        <v-row  no-gutters class="text-wrap noto-serif-display-bold text-h4 text-primary-lighten-3">
-          <v-col>{{ article.title }}</v-col>
-        </v-row>
-      </div>
-      <v-card-text class="ma-0 px-4 py-0 poppins-extralight text-body-2" >{{ article.description }}</v-card-text>
-      <v-row  no-gutters class="ma-0 px-4 py-2 text-wrap text-caption poppins-regular">
-          <v-col>
-            <FlexBox class="ga-2">
-              <div class="">{{ date.format(article.dates.published, 'fullDate') }}</div>
-              <span>|</span>
-              <v-icon icon="fa-solid fa-tag" size="small" color="secondary-lighten-3"></v-icon>
-              <FlexBox class="ga-1">
-                <div v-for="tag in article.tags" :key="tag">
-                  #{{ tag }}
-                </div>
-              </FlexBox>
-            </FlexBox>
-          </v-col>
+  <v-card class="my-2 main-background" @click="router.push(article._path)" variant="flat">
+    <div class="ma-0 px-4 py-2">
+      <v-row no-gutters class="text-caption poppins-regular pb-2">
+        <v-col class="d-flex flex-row  align-center">
+          <v-chip class="" label variant="outlined" density="comfortable" size="small" color="secondary">
+            {{ article.category }}
+          </v-chip>
+        </v-col>
       </v-row>
+      <v-row no-gutters class="text-wrap noto-serif-display-bold text-h4 text-primary">
+        <v-col>{{ article.title }}</v-col>
+      </v-row>
+    </div>
+    <v-card-text class="ma-0 px-4 py-0 poppins-extralight text-body-2">{{ article.description }}</v-card-text>
+    <v-row no-gutters class="ma-0 px-4 py-2 text-wrap text-caption poppins-regular">
+      <v-col>
+        <FlexBox class="ga-2">
+          <div class="">{{ date.format(article.dates.published, 'fullDate') }}</div>
+          <span>|</span>
+          <v-icon icon="fa-solid fa-tag" size="small" color="secondary"></v-icon>
+          <FlexBox class="ga-1">
+            <div v-for="tag in article.tags" :key="tag">
+              #{{ tag }}
+            </div>
+          </FlexBox>
+        </FlexBox>
+      </v-col>
+    </v-row>
   </v-card>
 </template>
 
-<style>
-
-</style>
+<style></style>
--- components/blog/ArticleToc.vue
+++ components/blog/ArticleToc.vue
@@ -2,15 +2,15 @@
 import FlexBox from '@/components/containers/FlexBox.vue';
 import { computed, ref } from 'vue';
 const props = defineProps({
-  links: {
-    type: Array,
-    nullable: true,
-    default: null
-  },
-  currentId:{
-    type: String,
-    default: null
-  }
+    links: {
+        type: Array,
+        nullable: true,
+        default: null
+    },
+    currentId: {
+        type: String,
+        default: null
+    }
 })
 
 const openToc = ref(true);
@@ -28,65 +28,61 @@ const toggleToc = computed(() => {
 </script>
 
 <template>
-  <v-navigation-drawer
-      location="right"
-
-      permanent
-      app
-      floating
-      width="250" style="position:fixed; top:0; right:0; overflow-y: auto; "
-      variant="plain"
-      class="main-background"
-  >
-      <div style="padding-top: 50px; text-align: right;">
-        <v-list-item>
-            <template v-slot:append>
-                <v-btn class="pa-0" icon="fa-solid fa-list" size="small" variant="plain" @click="openToc = !openToc"></v-btn>
-            </template>
-        </v-list-item>
-        <div :style="{ display: toggleToc }">
-            <v-list-item class="ma-3">
+    <v-navigation-drawer location="right" permanent app floating width="250"
+        style="position:fixed; top:0; right:0; overflow-y: auto; " variant="plain" class="main-background">
+        <div style="padding-top: 50px; text-align: right;">
+            <v-list-item>
                 <template v-slot:append>
-                    <FlexBox class="align-center ga-3">
-                        <v-icon icon="fa-solid fa-caret-up"></v-icon>
-                        <!-- <v-icon icon="fa-solid fa-caret-down"></v-icon> -->
-                        <v-icon icon="fa-solid fa-comment-dots"></v-icon>
-                    </FlexBox>
+                    <v-btn class="pa-0" icon="fa-solid fa-list" size="small" variant="plain"
+                        @click="openToc = !openToc"></v-btn>
                 </template>
             </v-list-item>
-            <v-list class="mr-3">
-                <div v-for="h2 in links"  :key="h2.text">
-                    <v-list-item density="compact" min-height="20" class="py-0">
-                        <v-list-item-title class="toc-h2 poppins-medium">
-                            <span class="h2-sharp text-primary-lighten-3"># </span> 
-                            <span :class="{ 'text-tertiary-lighten-2': h2.id == currentId? true: false, 'font-weight-bold': h2.id == currentId? true: false }">{{ h2.text }}</span>
-                        </v-list-item-title>
-                    </v-list-item>
-                    
-                    <div v-if="h2.children" class="">
-                        <div  v-for="h3 in h2.children" :key="h3.text">
-                            <v-list-item density="compact" min-height="20" class="py-0">
-                                <v-list-item-title class="toc-h3 poppins-regular"><span :class="{ 'text-tertiary-lighten-2': h3.id == currentId? true: false, 'font-weight-bold': h3.id == currentId? true: false }">{{ h3.text }}</span></v-list-item-title>
-                            </v-list-item>
+            <div :style="{ display: toggleToc }">
+                <v-list-item class="ma-3">
+                    <template v-slot:append>
+                        <FlexBox class="align-center ga-3">
+                            <v-icon icon="fa-solid fa-caret-up"></v-icon>
+                            <!-- <v-icon icon="fa-solid fa-caret-down"></v-icon> -->
+                            <v-icon icon="fa-solid fa-comment-dots"></v-icon>
+                        </FlexBox>
+                    </template>
+                </v-list-item>
+                <v-list class="mr-3">
+                    <div v-for="h2 in links" :key="h2.text">
+                        <v-list-item density="compact" min-height="20" class="py-0">
+                            <v-list-item-title class="toc-h2 poppins-medium">
+                                <span class="h2-sharp text-primary"># </span>
+                                <span
+                                    :class="{ 'text-tertiary': h2.id == currentId ? true : false, 'font-weight-bold': h2.id == currentId ? true : false }">{{ h2.text }}</span>
+                            </v-list-item-title>
+                        </v-list-item>
 
-                            <div v-if="h3.children">
-                                <div  v-for="h4 in h3.children" :key="h4.text">
-                                    <v-list-item density="compact" min-height="20" class="py-0" >
-                                        <v-list-item-title class="toc-h4 poppins-light">{{ h4.text }}</v-list-item-title>
-                                    </v-list-item>
+                        <div v-if="h2.children" class="">
+                            <div v-for="h3 in h2.children" :key="h3.text">
+                                <v-list-item density="compact" min-height="20" class="py-0">
+                                    <v-list-item-title class="toc-h3 poppins-regular"><span
+                                            :class="{ 'text-tertiary': h3.id == currentId ? true : false, 'font-weight-bold': h3.id == currentId ? true : false }">{{ h3.text }}</span></v-list-item-title>
+                                </v-list-item>
+
+                                <div v-if="h3.children">
+                                    <div v-for="h4 in h3.children" :key="h4.text">
+                                        <v-list-item density="compact" min-height="20" class="py-0">
+                                            <v-list-item-title
+                                                class="toc-h4 poppins-light">{{ h4.text }}</v-list-item-title>
+                                        </v-list-item>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            
-            </v-list>
+
+                </v-list>
+
+            </div>
+
 
         </div>
-        
-          
-      </div>
-  </v-navigation-drawer>
+    </v-navigation-drawer>
 </template>
 
 <style scoped>
@@ -102,7 +98,8 @@ const toggleToc = computed(() => {
 .toc-h3 {
     font-size: 11px;
 }
-.toc-h4{
+
+.toc-h4 {
     font-size: 9px;
 }
 </style>
