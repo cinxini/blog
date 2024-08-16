4fc49ff0fe64c9661bfe5736d1926011f54ba81f
Nutnaree Kleawsirikul <kleaws@wni.com>
2024-08-16 14:59:16 +0900

    update project list page

---
+++ content/project/project-2.md
@@ -0,0 +1,20 @@
+---
+title: Nuxt3 Blog
+author: "@nutnareek"
+dates:
+  published: "2024-06-24"
+description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tempus nibh libero. Cras porta quam vitae tortor sagittis pharetra. Phasellus ullamcorper orci ut lectus dapibus rhoncus. Ut facilisis tortor vitae commodo placerat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacinia suscipit aliquam.
+category: other
+tags: ["nuxt3", "nuxt-content", "project"]
+coverImage: 
+github: https://github.com/cinxini/dev-orange-cat
+status: 'published'
+isFeatured: true
+---
+
+# h1 Heading 8-)
+## h2 Heading
+### h3 Heading
+#### h4 Heading
+##### h5 Heading
+###### h6 Heading
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,11 +1,35 @@
 <script setup>
 import ProjectList from '@/components/ContentList.vue';
-import { computed, onMounted } from 'vue';
+import DotLoader from '@/components/items/DotLoader.vue';
 
-const fetchProjects = async () => {
+import c from '@/constants/posts';
+
+import { computed, onMounted, ref, watch } from 'vue';
+
+const isFetching = ref(false);
+const currPage = ref(1);
+const count = ref(0);
+
+const numPages = computed(() => {
+  if (count.value === 0) return 1;
+  return Math.ceil(count.value / c.POSTS_PER_PAGE);
+})
+
+const fetchProjects = async (pageNo) => {
+  isFetching.value = true;
   let data = null;
-  data = await queryContent('/project').sort({ 'dates.published': -1 }).find();
-  console.log
+  try {
+    data = await queryContent('/project')
+      .where({ status: 'published' })
+      .sort({ 'dates.published': -1 })
+      .skip((pageNo - 1) * c.POSTS_PER_PAGE)
+      .limit(c.POSTS_PER_PAGE)
+      .find();
+  } catch (error) {
+    console.log('Error:', error)
+  } finally {
+    isFetching.value = false;
+  }
   return data;
 }
 
@@ -21,20 +45,28 @@ const showList = computed(() => {
 })
 
 onMounted(async () => {
-  console.log(projectItems.value)
   if (!projectItems.value) {
-    projectItems.value = fetchProjects();
+    projectItems.value = fetchProjects(currPage.value);
   }
+  count.value = await queryContent('/project').where({ status: 'published' }).count()
 })
 
+watch(currPage, async (newPageNo) => {
+  projectItems.value = await fetchProjects(newPageNo);
+})
 
 </script>
 
 <template>
   <v-container class="w-66 main-container">
-    <p class="text-center text-h5 poppins-regular">Projects</p>
+    <p class="text-center text-h5">Recent Projects</p>
+    <div v-if="isFetching" class="d-flex flex-row justify-center ma-16">
+      <DotLoader />
+    </div>
     <div v-if="showList">
       <ProjectList :articles="projectItems"></ProjectList>
+      <v-pagination :length="numPages" v-model="currPage" next-icon="fa-solid fa-caret-right"
+        prev-icon="fa-solid fa-caret-left" rounded="lg" color="grey" active-color="primary"></v-pagination>
     </div>
   </v-container>
 </template>
